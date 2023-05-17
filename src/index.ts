c6f89dab60d6e26053ef9f2b7b3cea1c45c39887
Liubzzz <cosmin.liuba67@gmail.com>
2023-05-17 15:28:18 +0300

    changed some file names

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import sequelize from "./config/db-config";
-import Rooms from "./models/rooms";
+import Rooms from "./models/rooms.models";
 
 const app = express();
 
