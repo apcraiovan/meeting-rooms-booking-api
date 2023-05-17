2ba109a2be0c6c2143c2ffa0317b5228a9ea080f
GruescuDanziel <gruescu.ipatie@gmail.com>
2023-05-17 18:24:58 +0300

    Implemented CreateEvent and GetAllUsers

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('Beuty code comming to this proqject!');
+import { getUsers, postEvent} from "./outlookApi/outlookApi";
+import express from "express";
+
+require('dotenv').config()
+
+const app = express();
+
+app.use(express.json())
+
+app.post("/makeEvent", postEvent)
+app.get("/outlookUsers", getUsers)
+
+app.listen(3000)
