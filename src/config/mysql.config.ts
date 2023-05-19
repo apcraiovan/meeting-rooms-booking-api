cd72e76cfe0005ea9dd490cafbae13a39498a703
Liubzzz <cosmin.liuba67@gmail.com>
2023-05-19 16:20:59 +0300

    folder structure final version, remains to add tests

--- src/config/mysql.config.ts
+++ src/config/mysql.config.ts
@@ -1,29 +1,29 @@
 import { Sequelize } from "sequelize-typescript";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 import Users from "../models/user.entity";
 import Participants from "../models/participants.entity";
 dotenv.config(); // This should load the vars from .env file
 
-const DB_HOST = process.env.DB_HOST || 'localhost';
-const DB_USERNAME = process.env.DB_USERNAME || 'root'; 
-const DB_PASSWORD = process.env.DB_PASSWORD || 'Fanelu2002!';
-const DB_NAME = process.env.DB_NAME || 'test';
+const DB_HOST = process.env.DB_HOST || "localhost";
+const DB_USERNAME = process.env.DB_USERNAME || "root";
+const DB_PASSWORD = process.env.DB_PASSWORD || "root1234";
+const DB_NAME = process.env.DB_NAME || "mySQLInternshipApp";
 
 const sequelize = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
-    host: DB_HOST,
-    port: 4000,
-    logging: console.log,
-    dialect: 'mysql',
-    models: [Users,Participants]
+  host: DB_HOST,
+  port: 3306,
+  logging: console.log,
+  dialect: "mysql",
+  models: [Users, Participants],
 });
 
 async function connectToDatabase(): Promise<void> {
-    try {
-        await sequelize.authenticate();
-        console.log('Database connection established!');
-    } catch(err) {
-        console.error('Unable to connect to the database!', err);
-    }
+  try {
+    await sequelize.authenticate();
+    console.log("Database connection established!");
+  } catch (err) {
+    console.error("Unable to connect to the database!", err);
+  }
 }
 
 export { sequelize, connectToDatabase };
---
+++ src/dto/getAllRooms.dto.ts
@@ -0,0 +1,6 @@
+export interface RoomAttributesDto {
+  id: number;
+  name: string;
+  capacity: number;
+  description: string;
+}
