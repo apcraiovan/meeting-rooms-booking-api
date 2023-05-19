fc3517febdd78be3ee93017fe1b6f3debc1e2c4f
Cristian Berzescu <cristianberzescu@Cristians-MacBook-Pro.local>
2023-05-19 16:57:20 +0300

    meetings table

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import {sequelize} from "./config/mysql.config";
 import app from "./app";
+import Meeting from "./models/meeting.entity";
+import MeetingService from "./service/meeting.service";
+
+const MeetingServ = new MeetingService()
 
 sequelize
   .authenticate()
@@ -9,6 +13,42 @@ sequelize
       .sync()
       .then(async() => {
         console.log("success");
+
+
+        const meetToAdd = [
+          {
+            name: "intalnire",
+            description: "descriere",
+            startTime: new Date(),
+            endTime: new Date(),
+            roomId: 2
+          },
+         
+        ];
+
+        // Meeting.bulkCreate(meetToAdd)
+        //   .then(() => {
+        //     console.log("meet created successfully");
+        //   })
+        //   .catch((err: Error) => {
+        //     console.log("Error creating meet: ", err);
+        //   });
+
+
+          const testFunctionGetMeeting = async () => {
+            try {
+              const meetings = await MeetingServ.GetAllMeetingsByRoomId(22);
+              //const meetings = await MeetingServ.GetMeetingById(4);
+  
+              console.log(meetings);
+            } catch (error) {
+              console.log(error);
+            }
+          };
+  
+          testFunctionGetMeeting();
+
+
       })
       .catch((err: Error) => {
         console.log(err);
