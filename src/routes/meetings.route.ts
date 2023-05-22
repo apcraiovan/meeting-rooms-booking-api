793872394ebb6e1820d0937914915b8b10447cbe
Fercal Alex <alex.fercal6@gmail.com>
2023-05-22 16:17:44 +0300

    A

--- src/routes/meetings.route.ts
+++ src/routes/meetings.route.ts
@@ -1,6 +1,9 @@
 export {};
 const express = require("express");
+import { NotEmpty } from "sequelize-typescript";
 import { MeetingController } from "../controller/meetings.controller";
+import {check} from "express-validator";
+
 
 const meetingController = new MeetingController();
 const meetingRouter = express.Router();
@@ -8,6 +11,14 @@ const meetingRouter = express.Router();
 //MEETINGS ROUTER
 meetingRouter.post("", meetingController.postMeeting);
 
-meetingRouter.get("/:id", meetingController.getMeetings);
+meetingRouter.get("/:id",
+[
+    check('id').notEmpty().isNumeric(),
+    check('name').notEmpty().isString(),
+    check('description').notEmpty().isString(),
+    check('startTime').notEmpty().toDate().isDate(),
+    check('endTime').notEmpty().isString()
+], 
+meetingController.getMeetings);
 
 module.exports = meetingRouter;
