dccbc4fe852ef3f16c6386af8513f680f113d854
Liubzzz <cosmin.liuba67@gmail.com>
2023-05-23 03:47:03 +0300

    added mock tests to the final backend code

--- src/models/participants.entity.ts
+++ src/models/participants.entity.ts
@@ -1,12 +1,39 @@
+// import {
+//   DataType,
+//   Table,
+//   Column,
+//   Model,
+//   ForeignKey,
+// } from "sequelize-typescript";
+// import Users from "./user.entity";
+// import Meeting from "./meeting.entity";
+// @Table({})
+// class Participants extends Model {
+//   @Column({
+//     type: DataType.INTEGER,
+//     primaryKey: true,
+//     allowNull: false,
+//   })
+//   id!: number;
+//   @ForeignKey(() => Users)
+//   @Column
+//   userId!: number;
+//   @ForeignKey(() => Meeting)
+//   @Column
+//   meetId!: number;
+// }
+// export default Participants;
+
 import {
-  DataType,
-  Table,
   Column,
-  Model,
+  DataType,
   ForeignKey,
+  Model,
+  Table,
 } from "sequelize-typescript";
 import Users from "./user.entity";
 import Meeting from "./meeting.entity";
+
 @Table({})
 class Participants extends Model {
   @Column({
@@ -15,11 +42,22 @@ class Participants extends Model {
     allowNull: false,
   })
   id!: number;
+
   @ForeignKey(() => Users)
-  @Column
+  @Column({
+    type: DataType.INTEGER, // Define the data type as INTEGER
+    allowNull: false,
+  })
   userId!: number;
+
   @ForeignKey(() => Meeting)
-  @Column
+  @Column({
+    type: DataType.INTEGER, // Define the data type as INTEGER
+    allowNull: false,
+  })
   meetId!: number;
+
+  // Other properties and associations
 }
+
 export default Participants;
