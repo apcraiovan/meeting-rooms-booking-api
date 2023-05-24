d00e2dba05bff1e2a506f544aced6bd120ae776f
Liubzzz <cosmin.liuba67@gmail.com>
2023-05-24 11:31:34 +0300

    some changes

--- src/config/mysql.config.ts
+++ src/config/mysql.config.ts
@@ -6,8 +6,8 @@ dotenv.config(); // This should load the vars from .env file
 
 const DB_HOST = process.env.DB_HOST || "localhost";
 const DB_USERNAME = process.env.DB_USERNAME || "root";
-const DB_PASSWORD = process.env.DB_PASSWORD || "doctari31";
-const DB_NAME = process.env.DB_NAME || "new_schema";
+const DB_PASSWORD = process.env.DB_PASSWORD || "root1234";
+const DB_NAME = process.env.DB_NAME || "mySQLInternshipApp";
 
 const sequelize = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
   host: DB_HOST,
